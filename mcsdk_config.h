dcd949a8ca5a457b9805248c213bd4e5aaed748d
chyves <82107846+notchyves@users.noreply.github.com>
2024-11-21 15:16:32 -0500

    Create mcsdk_config.h

---
+++ mcsdk_config.h
@@ -0,0 +1,9 @@
+// Purpose: To allow people to bring their own Memory Utils and whatnot to the table if needed
+
+#pragma once
+
+namespace mcsdk {
+	namespace detail {
+		// findSignature() ...
+  	};
+};
